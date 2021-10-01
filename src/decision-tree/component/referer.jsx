c9d10dd2bc4ba1decc3a8d07311677c9a513acc5
stan turner <stanturner@MacBook-Pro-van-stan.local>
2021-10-01 11:40:29 +0200

    Changed label with link of decision tree referer

--- src/decision-tree/component/referer.jsx
+++ src/decision-tree/component/referer.jsx
@@ -1,13 +1,9 @@
 import { Explainer } from "./explainer.jsx";
 
 export function Referer({ activeDecision }) {
-
   return (
     <div>
       <Explainer activeDecision={activeDecision} />
-      <p>
-        <a href={activeDecision.url} target="_blank">{activeDecision.url}</a>
-      </p>
     </div>
   );
 }
